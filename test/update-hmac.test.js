b9ce3b79c5f8015639aa7581a12a2dd911667bca
tsmx <strs@freenet.de>
2021-10-05 09:18:55 +0200

    Unit-tests for update feature completed

--- test/update-hmac.test.js
+++ test/update-hmac.test.js
@@ -48,4 +48,37 @@ describe('secure-config-tool update-hmac test suite', () => {
         expect(updatedJson['__hmac']).toStrictEqual(oh.calculateHmac(originalJson, TEST_KEY_HEX));
     });
 
+    it('tests a successful HMAC update with a custom HMAC property', () => {
+        process.env['CONFIG_ENCRYPTION_KEY'] = TEST_KEY_HEX;
+        const updateHmac = require('../functions/update-hmac');
+        const customProp = '_signature';
+        updateHmac('./test/testfiles/config-hmac-prop-update.json', { hmacProp: customProp });
+        expect(testOutput.length).toBe(1);
+        let updatedJson = JSON.parse(testOutput[0]);
+        let originalJson = JSON.parse(fs.readFileSync('./test/testfiles/config-hmac-prop-update.json'));
+        expect(updatedJson.database.host).toStrictEqual(originalJson.database.host);
+        expect(updatedJson.database.user).toStrictEqual(originalJson.database.user);
+        expect(updatedJson.database.pass).toStrictEqual(originalJson.database.pass);
+        expect(updatedJson.database.port).toStrictEqual(originalJson.database.port);
+        expect(updatedJson['__hmac']).toBeUndefined();
+        expect(originalJson['__hmac']).toBeUndefined();
+        expect(updatedJson[customProp]).toBeDefined();
+        expect(originalJson[customProp]).toBeDefined();
+        expect(updatedJson[customProp]).not.toStrictEqual(originalJson[customProp]);
+        jt.traverse(originalJson, cbDecrypt, true);
+        delete originalJson[customProp];
+        expect(updatedJson[customProp]).toStrictEqual(oh.calculateHmac(originalJson, TEST_KEY_HEX));
+    });
+
+    it('tests a failed file HMAC update because of a missing key', () => {
+        const mockExit = jest.spyOn(process, 'exit')
+            .mockImplementation((number) => { throw new Error('process.exit: ' + number); });
+        const updateHmac = require('../functions/update-hmac');
+        expect(() => {
+            updateHmac('./test/testfiles/config-hmac-update.json');
+        }).toThrow();
+        expect(mockExit).toHaveBeenCalledWith(-1);
+        mockExit.mockRestore();
+    });
+
 });
