948ca9d860f6494c366cf28ea7e2931846501308
tsmx <dev@tsmx.net>
2024-08-08 21:38:51 +0200

    Upgraded to eslint 9.8, publish actions v4

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+const { configs } = require('@eslint/js');
+const jest = require('eslint-plugin-jest');
+const globals = require('globals');
+
+module.exports = [
+    configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'commonjs',
+            globals: { ...globals.node, ...globals.jest, Atomics: 'readonly', SharedArrayBuffer: 'readonly' }
+        },
+        rules: {
+            semi: 'error',
+            quotes: ['error', 'single'],
+            indent: ['error', 4, { 'SwitchCase': 1 }],
+            'no-unused-vars':
+                [
+                    'warn',
+                    {
+                        'varsIgnorePattern': '^_',
+                        'args': 'after-used',
+                        'argsIgnorePattern': '^_'
+                    }
+                ]
+        },
+        ignores: ['coverage/', '**/*.json']
+    },
+    {
+        languageOptions: {
+            globals: { ...globals.jest }
+        },
+        files: ['test/*.test.js'],
+        ...jest.configs['flat/recommended'],
+        rules: {
+            ...jest.configs['flat/recommended'].rules
+        }
+    }
+];
