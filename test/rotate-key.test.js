f8e2765ff2fb34427eb5a7ab634b259494cd8459
tsmx <dev@tsmx.net>
2024-12-13 22:21:45 +0100

    Key rotation test improved

--- test/rotate-key.test.js
+++ test/rotate-key.test.js
@@ -8,6 +8,7 @@ describe('secure-config-tool rotate-key test suite', () => {
     const testConsoleLog = (output) => { testOutput.push(output); };
 
     const TEST_KEY_HEX_OLD = '9af7d400be4705147dc724db25bfd2513aa11d6013d7bf7bdb2bfe050593bd0f';
+    const TEST_KEY_HEX_OLD_WRONG = '9af7d400be4705147dc724db25bfd2513aa11d6013d7bf7bdb2bfe050593bdaa';
     const TEST_KEY_HEX_NEW = '9af7d400be4705147dc724db25bfd2513aa11d6013d7bf7bdb2bfe050593b000';
 
     beforeEach(() => {
@@ -65,6 +66,37 @@ describe('secure-config-tool rotate-key test suite', () => {
         expect(cryptUtils.decrypt(updatedJson.database.password, TEST_KEY_HEX_NEW)).toStrictEqual(originalConfig.database.password);
         expect(updatedJson['__hmac']).toBeDefined();
         expect(updatedJson['__hmac']).toStrictEqual(oh.calculateHmac(originalConfig, TEST_KEY_HEX_NEW));
+    });
+
+    it('tests a successful key rotation with custom HMAC property name', () => {
+        process.env[cryptUtils.CONFIG_ENCRYPTION_KEY] = TEST_KEY_HEX_OLD;
+        process.env[cryptUtils.CONFIG_ENCRYPTION_KEY_NEW] = TEST_KEY_HEX_NEW;
+        const customHmacProp = '_signature';
+        const rotateKey = require('../functions/rotate-key');
+        rotateKey('./test/testfiles/config-test-hmacprop.json', { hmacProp: customHmacProp });
+        expect(testOutput.length).toBe(1);
+        let updatedJson = JSON.parse(testOutput[0]);
+        const originalConfig = require('./testfiles/config-test-plain.json');
+        expect(updatedJson.database.host).toStrictEqual(originalConfig.database.host);
+        expect(updatedJson.database.username).toBeDefined();
+        expect(cryptUtils.decrypt(updatedJson.database.username, TEST_KEY_HEX_NEW)).toStrictEqual(originalConfig.database.username);
+        expect(updatedJson.database.password).toBeDefined();
+        expect(cryptUtils.decrypt(updatedJson.database.password, TEST_KEY_HEX_NEW)).toStrictEqual(originalConfig.database.password);
+        expect(updatedJson[customHmacProp]).toBeDefined();
+        expect(updatedJson[customHmacProp]).toStrictEqual(oh.calculateHmac(originalConfig, TEST_KEY_HEX_NEW));
+    });
+
+    it('tests a failed key rotation because of wrong old key', () => {
+        process.env[cryptUtils.CONFIG_ENCRYPTION_KEY] = TEST_KEY_HEX_OLD_WRONG;
+        process.env[cryptUtils.CONFIG_ENCRYPTION_KEY_NEW] = TEST_KEY_HEX_NEW;
+        const mockExit = jest.spyOn(process, 'exit')
+            .mockImplementation((number) => { throw new Error('process.exit: ' + number); });
+        const rotateKey = require('../functions/rotate-key');
+        expect(() => {
+            rotateKey('./test/testfiles/config-test.json');
+        }).toThrow();
+        expect(mockExit).toHaveBeenCalledWith(-1);
+        mockExit.mockRestore();
     });
 
 });
